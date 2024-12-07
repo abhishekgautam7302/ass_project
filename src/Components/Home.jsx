6a4f0730dcb39bc504edffdd38057a0c187276b6
Abhishek Gautam <abhishekgautam730249@gmail.com>
2024-12-07 21:56:41 +0530

    update home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='bg-gradient-to-r from-violet-600 to-indigo-600 w-full h-screen'>
-        <h1 className='text-3xl font-bold text-white flex justify-center py-80'> welcome Your Our Home Page</h1>
+        <h1 className='text-3xl font-bold text-white flex justify-center py-80'> welcome Your Our Home Page Dashboard</h1>
     </div>
   )
 }
